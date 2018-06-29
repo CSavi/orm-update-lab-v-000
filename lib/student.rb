d09e4983241f8752cd5e30fd306b03e4c9599ce8
Carrie Savinell <carrielane89@gmail.com>
2018-06-29 15:03:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,17 @@ class Student
     new_student
   end
 
-  def self.new_from_db
+  #convert data from database into ruby object
+  def self.new_from_db(row)
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.find_by_name(name)
-    
+
   end
 
   def update
